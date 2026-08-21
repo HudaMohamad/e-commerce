28a7bc4c287f812ad75b9f0f8f92e1a02b836ee0
Huda Abou AlKherr <huda.abou.alkherr@example.com>
2026-08-21 11:50:02 +0300

    Fix routing for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { CartProvider } from './components/context/CartContext.jsx';   // است
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/e-commerce">
       <CartProvider>
         <App />
       </CartProvider>
